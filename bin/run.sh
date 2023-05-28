5d2af55506dbd3a80b1181640f3ab9aed5868a2b
Andrii Zagoruiko <andrii.zagoruiko@oracle.com>
2023-05-28 18:12:06 +0200

    fix s3 loader

--- bin/run.sh
+++ bin/run.sh
@@ -7,13 +7,13 @@ echo "RUNNING MASTER $SPARK_MASTER"
   --master spark://$SPARK_MASTER \
   --deploy-mode client \
   --conf spark.sql.catalogImplementation=hive \
-  --conf spark.hadoop.fs.s3a.path.style.access=false \
+  --conf spark.hadoop.fs.s3a.path.style.access=true \
   --conf spark.hadoop.datanucleus.autoCreateSchema=true \
   --conf spark.hadoop.datanucleus.autoCreateTables=true \
   --conf spark.hadoop.javax.jdo.option.ConnectionURL=${POSTGRES_METASTORE_JDBC_URL} \
   --conf spark.hadoop.javax.jdo.option.ConnectionDriverName=${POSTGRES_JDBC_DRIVER} \
   --conf spark.hadoop.javax.jdo.option.ConnectionUserName=${POSTGRES_JDBC_USER} \
   --conf spark.hadoop.javax.jdo.option.ConnectionPassword=${POSTGRES_JDBC_PASSWORD} \
-  --conf spark.executor.instances=2 \
+  --conf spark.executor.instances=3 \
   --conf spark.cores.max=6 \
   $S3_SHARED_BUCKET/expenses.jar
