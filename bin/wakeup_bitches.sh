ac384aefbf0d600e7e0e6bf3afe51e5160f91cd3
Andrii Zagoruiko <andrii.zagoruiko@oracle.com>
2023-11-26 18:31:52 +0100

    Fix DBService bean resolving

---
+++ bin/wakeup_bitches.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Wake up bitch 94:c6:91:a1:39:00"
+etherwake -i enp3s0 94:c6:91:a1:39:00
+
+echo "Wake up bitch 94:c6:91:a0:fe:07"
+etherwake -i enp3s0 94:c6:91:a0:fe:07
+
+echo "Wkae up bitch 94:c6:91:a7:bd:24"
+etherwake -i enp3s0 94:c6:91:a7:bd:24
